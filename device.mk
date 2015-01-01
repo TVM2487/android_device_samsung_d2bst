7943a1548fadbec9c0f11d31c718ebd9141832c2
Tony Malagisi <mtb3000gt@gmail.com>
2015-01-01 09:16:56 -0500

    Use Boost Mobile Proprietary Files
    Change-Id: If0fbde6e9181eb68a27dee25631ce93b66e132f3

--- device.mk
+++ device.mk
@@ -15,7 +15,7 @@
 #
 
 ## (2) Also get non-open-source specific aspects if available
-$(call inherit-product-if-exists, vendor/samsung/d2spr/d2spr-vendor.mk)
+$(call inherit-product-if-exists, vendor/samsung/d2bst/d2bst-vendor.mk)
 
 ## Inherit d2bst overlays
 DEVICE_PACKAGE_OVERLAYS += device/samsung/d2bst/overlay
