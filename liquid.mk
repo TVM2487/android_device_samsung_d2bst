b90f97b69413df5058393d7dc856278e201487cf
Tony Malagisi <mtb3000gt@gmail.com>
2015-01-07 16:58:42 -0500

    Update Fingerprint to 5.0.2
    Change-Id: I7ac7fad6b447b93ae770d4311b262bbdb5b2099e

--- liquid.mk
+++ liquid.mk
@@ -9,8 +9,8 @@ $(call inherit-product, vendor/liquid/config/common_phone.mk)
 PRODUCT_BUILD_PROP_OVERRIDES += \
     PRODUCT_NAME=d2bst \
     TARGET_DEVICE=d2bst \
-    BUILD_FINGERPRINT="samsung/d2bst/d2bst:4.4.2/KOT49H/L710TVPU0ANF4:user/release-keys" \
-    PRIVATE_BUILD_DESC="d2bst-user 4.4.2 KOT49H L710TVPU0ANF4 release-keys"
+    BUILD_FINGERPRINT="samsung/d2bst/d2bst:5.0.2/LRX22G/L710TVPU0ANF4:user/release-keys" \
+    PRIVATE_BUILD_DESC="d2bst-user 5.0.2 LRX22G L710TVPU0ANF4 release-keys"
 
 PRODUCT_NAME := liquid_d2bst
 PRODUCT_DEVICE := d2bst
